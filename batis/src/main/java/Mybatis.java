e1256d8bc3966f178bb34c3cb468d0904bc8b85e
groundsnail <52134052+groundsnail@users.noreply.github.com>
2021-06-27 14:42:53 +0800

    Initial commit

---
+++ batis/src/main/java/Mybatis.java
@@ -0,0 +1,20 @@
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.List;
+
+public class Mybatis {
+    public static void main(String[] args) throws IOException {
+        InputStream resourceAsStream = Resources.getResourceAsStream("mybatis_config.xml");
+        SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(resourceAsStream);
+        SqlSession sqlSession = sqlSessionFactory.openSession();
+        List<User> userList = sqlSession.selectList("userMapper.selectBlog");
+        System.out.println(userList);
+
+        sqlSession.close();
+    }
+}
---
+++ batis/src/test/java/MbTest.java
@@ -0,0 +1,27 @@
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.List;
+
+public class MbTest {
+    @Test
+    public void test() throws IOException {
+        InputStream resourceAsStream = Resources.getResourceAsStream("mybatis_config.xml");
+        SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(resourceAsStream);
+        SqlSession sqlSession = sqlSessionFactory.openSession();
+        List<User> userList = sqlSession.selectList("userMapper.selectBlog");
+        System.out.println(userList);
+//        UserMap mapper = sqlSession.getMapper(UserMap.class);
+//        User userById = mapper.getUserById(1);
+//        System.out.println(userById);
+        sqlSession.close();
+
+
+    }
+
+}
